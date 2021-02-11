9da181067457a1c011042a544d6a16d40180add5
arpit1992-github <arpitmalviya.eng@gmail.com>
2021-02-12 04:14:38 +0530

    msg

--- src/main/java/com/example/demo/LibraryApplication.java
+++ src/main/java/com/example/demo/LibraryApplication.java
@@ -12,7 +12,6 @@ public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
-		System.out.println("Hi");
 	}
 	@Bean
 	public ModelMapper getModelMapper() {
